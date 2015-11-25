8c768ae6a5e36020a7bfc260499b9b675a8954fe
Volodymyr Tovazhnianskyi <volodymyr_tovazhnianskyi@epam.com>
2015-11-25 17:58:25 +0200

    Creating tile generator

---
+++ Game/Game/Form1.Designer.cs
@@ -0,0 +1,301 @@
+﻿namespace Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Tile_1_1 = new System.Windows.Forms.PictureBox();
+            this.Tile_1_2 = new System.Windows.Forms.PictureBox();
+            this.Tile_1_3 = new System.Windows.Forms.PictureBox();
+            this.Tile_1_4 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.Tile_2_4 = new System.Windows.Forms.PictureBox();
+            this.Tile_2_3 = new System.Windows.Forms.PictureBox();
+            this.Tile_2_2 = new System.Windows.Forms.PictureBox();
+            this.Tile_2_1 = new System.Windows.Forms.PictureBox();
+            this.Tile_4_4 = new System.Windows.Forms.PictureBox();
+            this.Tile_4_3 = new System.Windows.Forms.PictureBox();
+            this.Tile_4_2 = new System.Windows.Forms.PictureBox();
+            this.Tile_4_1 = new System.Windows.Forms.PictureBox();
+            this.Tile_3_4 = new System.Windows.Forms.PictureBox();
+            this.Tile_3_3 = new System.Windows.Forms.PictureBox();
+            this.Tile_3_2 = new System.Windows.Forms.PictureBox();
+            this.Tile_3_1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Tile_1_1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Tile_1_2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Tile_1_3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Tile_1_4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Tile_2_4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Tile_2_3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Tile_2_2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Tile_2_1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Tile_4_4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Tile_4_3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Tile_4_2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Tile_4_1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Tile_3_4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Tile_3_3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Tile_3_2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Tile_3_1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Tile_1_1
+            // 
+            this.Tile_1_1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.Tile_1_1.Location = new System.Drawing.Point(398, 143);
+            this.Tile_1_1.Name = "Tile_1_1";
+            this.Tile_1_1.Size = new System.Drawing.Size(100, 100);
+            this.Tile_1_1.TabIndex = 0;
+            this.Tile_1_1.TabStop = false;
+            this.Tile_1_1.Click += new System.EventHandler(this.Tile_Click);
+            // 
+            // Tile_1_2
+            // 
+            this.Tile_1_2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Tile_1_2.Location = new System.Drawing.Point(498, 143);
+            this.Tile_1_2.Name = "Tile_1_2";
+            this.Tile_1_2.Size = new System.Drawing.Size(100, 100);
+            this.Tile_1_2.TabIndex = 1;
+            this.Tile_1_2.TabStop = false;
+            this.Tile_1_2.Click += new System.EventHandler(this.Tile_Click);
+            // 
+            // Tile_1_3
+            // 
+            this.Tile_1_3.BackColor = System.Drawing.SystemColors.Highlight;
+            this.Tile_1_3.Location = new System.Drawing.Point(598, 143);
+            this.Tile_1_3.Name = "Tile_1_3";
+            this.Tile_1_3.Size = new System.Drawing.Size(100, 100);
+            this.Tile_1_3.TabIndex = 2;
+            this.Tile_1_3.TabStop = false;
+            this.Tile_1_3.Click += new System.EventHandler(this.Tile_Click);
+            // 
+            // Tile_1_4
+            // 
+            this.Tile_1_4.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.Tile_1_4.Location = new System.Drawing.Point(698, 143);
+            this.Tile_1_4.Name = "Tile_1_4";
+            this.Tile_1_4.Size = new System.Drawing.Size(100, 100);
+            this.Tile_1_4.TabIndex = 3;
+            this.Tile_1_4.TabStop = false;
+            this.Tile_1_4.Click += new System.EventHandler(this.Tile_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(210, 453);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(123, 45);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "TO THE RIGHT";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(59, 453);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(123, 45);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "TO THE LEFT";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Tile_2_4
+            // 
+            this.Tile_2_4.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.Tile_2_4.Location = new System.Drawing.Point(698, 243);
+            this.Tile_2_4.Name = "Tile_2_4";
+            this.Tile_2_4.Size = new System.Drawing.Size(100, 100);
+            this.Tile_2_4.TabIndex = 9;
+            this.Tile_2_4.TabStop = false;
+            // 
+            // Tile_2_3
+            // 
+            this.Tile_2_3.BackColor = System.Drawing.SystemColors.Highlight;
+            this.Tile_2_3.Location = new System.Drawing.Point(598, 243);
+            this.Tile_2_3.Name = "Tile_2_3";
+            this.Tile_2_3.Size = new System.Drawing.Size(100, 100);
+            this.Tile_2_3.TabIndex = 8;
+            this.Tile_2_3.TabStop = false;
+            // 
+            // Tile_2_2
+            // 
+            this.Tile_2_2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Tile_2_2.Location = new System.Drawing.Point(498, 243);
+            this.Tile_2_2.Name = "Tile_2_2";
+            this.Tile_2_2.Size = new System.Drawing.Size(100, 100);
+            this.Tile_2_2.TabIndex = 7;
+            this.Tile_2_2.TabStop = false;
+            // 
+            // Tile_2_1
+            // 
+            this.Tile_2_1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.Tile_2_1.Location = new System.Drawing.Point(398, 243);
+            this.Tile_2_1.Name = "Tile_2_1";
+            this.Tile_2_1.Size = new System.Drawing.Size(100, 100);
+            this.Tile_2_1.TabIndex = 6;
+            this.Tile_2_1.TabStop = false;
+            // 
+            // Tile_4_4
+            // 
+            this.Tile_4_4.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.Tile_4_4.Location = new System.Drawing.Point(698, 443);
+            this.Tile_4_4.Name = "Tile_4_4";
+            this.Tile_4_4.Size = new System.Drawing.Size(100, 100);
+            this.Tile_4_4.TabIndex = 17;
+            this.Tile_4_4.TabStop = false;
+            // 
+            // Tile_4_3
+            // 
+            this.Tile_4_3.BackColor = System.Drawing.SystemColors.Highlight;
+            this.Tile_4_3.Location = new System.Drawing.Point(598, 443);
+            this.Tile_4_3.Name = "Tile_4_3";
+            this.Tile_4_3.Size = new System.Drawing.Size(100, 100);
+            this.Tile_4_3.TabIndex = 16;
+            this.Tile_4_3.TabStop = false;
+            // 
+            // Tile_4_2
+            // 
+            this.Tile_4_2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Tile_4_2.Location = new System.Drawing.Point(498, 443);
+            this.Tile_4_2.Name = "Tile_4_2";
+            this.Tile_4_2.Size = new System.Drawing.Size(100, 100);
+            this.Tile_4_2.TabIndex = 15;
+            this.Tile_4_2.TabStop = false;
+            // 
+            // Tile_4_1
+            // 
+            this.Tile_4_1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.Tile_4_1.Location = new System.Drawing.Point(398, 443);
+            this.Tile_4_1.Name = "Tile_4_1";
+            this.Tile_4_1.Size = new System.Drawing.Size(100, 100);
+            this.Tile_4_1.TabIndex = 14;
+            this.Tile_4_1.TabStop = false;
+            // 
+            // Tile_3_4
+            // 
+            this.Tile_3_4.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.Tile_3_4.Location = new System.Drawing.Point(698, 343);
+            this.Tile_3_4.Name = "Tile_3_4";
+            this.Tile_3_4.Size = new System.Drawing.Size(100, 100);
+            this.Tile_3_4.TabIndex = 13;
+            this.Tile_3_4.TabStop = false;
+            // 
+            // Tile_3_3
+            // 
+            this.Tile_3_3.BackColor = System.Drawing.SystemColors.Highlight;
+            this.Tile_3_3.Location = new System.Drawing.Point(598, 343);
+            this.Tile_3_3.Name = "Tile_3_3";
+            this.Tile_3_3.Size = new System.Drawing.Size(100, 100);
+            this.Tile_3_3.TabIndex = 12;
+            this.Tile_3_3.TabStop = false;
+            // 
+            // Tile_3_2
+            // 
+            this.Tile_3_2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Tile_3_2.Location = new System.Drawing.Point(498, 343);
+            this.Tile_3_2.Name = "Tile_3_2";
+            this.Tile_3_2.Size = new System.Drawing.Size(100, 100);
+            this.Tile_3_2.TabIndex = 11;
+            this.Tile_3_2.TabStop = false;
+            // 
+            // Tile_3_1
+            // 
+            this.Tile_3_1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.Tile_3_1.Location = new System.Drawing.Point(398, 343);
+            this.Tile_3_1.Name = "Tile_3_1";
+            this.Tile_3_1.Size = new System.Drawing.Size(100, 100);
+            this.Tile_3_1.TabIndex = 10;
+            this.Tile_3_1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(984, 961);
+            this.Controls.Add(this.Tile_4_4);
+            this.Controls.Add(this.Tile_4_3);
+            this.Controls.Add(this.Tile_4_2);
+            this.Controls.Add(this.Tile_4_1);
+            this.Controls.Add(this.Tile_3_4);
+            this.Controls.Add(this.Tile_3_3);
+            this.Controls.Add(this.Tile_3_2);
+            this.Controls.Add(this.Tile_3_1);
+            this.Controls.Add(this.Tile_2_4);
+            this.Controls.Add(this.Tile_2_3);
+            this.Controls.Add(this.Tile_2_2);
+            this.Controls.Add(this.Tile_2_1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Tile_1_4);
+            this.Controls.Add(this.Tile_1_3);
+            this.Controls.Add(this.Tile_1_2);
+            this.Controls.Add(this.Tile_1_1);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            ((System.ComponentModel.ISupportInitialize)(this.Tile_1_1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Tile_1_2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Tile_1_3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Tile_1_4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Tile_2_4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Tile_2_3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Tile_2_2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Tile_2_1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Tile_4_4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Tile_4_3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Tile_4_2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Tile_4_1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Tile_3_4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Tile_3_3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Tile_3_2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Tile_3_1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox Tile_1_1;
+        private System.Windows.Forms.PictureBox Tile_1_2;
+        private System.Windows.Forms.PictureBox Tile_1_3;
+        private System.Windows.Forms.PictureBox Tile_1_4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox Tile_2_4;
+        private System.Windows.Forms.PictureBox Tile_2_3;
+        private System.Windows.Forms.PictureBox Tile_2_2;
+        private System.Windows.Forms.PictureBox Tile_2_1;
+        private System.Windows.Forms.PictureBox Tile_4_4;
+        private System.Windows.Forms.PictureBox Tile_4_3;
+        private System.Windows.Forms.PictureBox Tile_4_2;
+        private System.Windows.Forms.PictureBox Tile_4_1;
+        private System.Windows.Forms.PictureBox Tile_3_4;
+        private System.Windows.Forms.PictureBox Tile_3_3;
+        private System.Windows.Forms.PictureBox Tile_3_2;
+        private System.Windows.Forms.PictureBox Tile_3_1;
+    }
+}
+
---
+++ Game/Game/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Game
+{
+
+    public partial class Form1 : Form
+    {
+        private List<PictureBox> listOfTiles = new List<PictureBox>();
+
+        private PictureBox[,] arrayOfTiles = new PictureBox[4,4];
+        Random rnd = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            foreach (var tile in listOfTiles)
+            {
+                tile.Left += 100;
+            }
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            //            listOfTiles.Add(Tile_1_1);
+            //            listOfTiles.Add(Tile_1_2);
+            //            listOfTiles.Add(Tile_1_3);
+            //            listOfTiles.Add(Tile_1_4);
+            //int i = 0;
+            //int j = 0;
+            //foreach (var pb in this.Controls.OfType<PictureBox>())
+            //{
+            //    listOfTiles.Add(pb);
+            //    try
+            //    {
+            //        arrayOfTiles[j][i++] = pb;
+            //    }
+            //    catch (NullReferenceException ex)
+            //    {
+
+            //    }
+
+
+            //    if (i == 4)
+            //    {
+            //        j++;
+            //        i = 0;
+            //    }
+            //    pb.Click += new EventHandler(Tile_Click);
+            //}
+            createTileSet(6, 1);
+        }
+
+        private void Tile_Click(object sender, EventArgs e)
+        {
+            PictureBox tile = sender as PictureBox;
+
+            if (tile != null)
+            {
+                tile.BackColor = Color.FromArgb(rnd.Next(255), rnd.Next(255), rnd.Next(255));
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            foreach (var tile in listOfTiles)
+            {
+                tile.Left -= 100;
+            }
+        }
+
+        private void createTileSet(int verticalSize, int horizontalSize)
+        {
+
+            for (int i=0; i<verticalSize; i++)
+                for (int j = 0; j < horizontalSize; j++)
+                {
+                    //Button pb = new Button();
+                    PictureBox pb = new PictureBox();
+                    //pb.Location = new Point(20, 30 * i + 10);
+                    pb.Name = "Tileset_" + j + "_" + i;
+                    pb.Visible = true;
+                    pb.Location = new Point(100 * i, 100 * j);
+                    pb.Size = new Size(100, 100);
+                    pb.BackColor = Color.FromArgb(rnd.Next(255), rnd.Next(255), rnd.Next(255));
+                    try
+                    {
+                        arrayOfTiles[j,i] = pb;
+                    }
+                    catch(Exception ex)
+                    {
+
+                    }
+                    
+                    pb.Click += new EventHandler(Tile_Click);
+                    Controls.Add(pb);
+                }
+        }
+    }
+}
